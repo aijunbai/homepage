66981b68b8a0022514697bef5e18cd10ebbdb37d
Aijun Bai <aijunbai@gmail.com>
2016-06-08 11:56:58 -0700

    updated

--- wrighteagle2d/list_robocup_tdps.sh
+++ wrighteagle2d/list_robocup_tdps.sh
@@ -8,7 +8,7 @@ touch $OUTPUT
 
 echo "<p>This is a list of Team Description Papers (TDPs) from annual RoboCup 2D competitions since 2002.</p>" >> $OUTPUT
 i=0
-for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d | sort`; do
+for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d | sort -r`; do
   path=`basename "$filepath"`
   echo "  <H2>$path</H2>" >> $OUTPUT
   echo "  <UL>" >> $OUTPUT
