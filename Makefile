36b5436852c6b2b97e499ded31919427db6151ea
Aijun Bai <aijunbai@gmail.com>
2016-06-21 18:10:58 -0700

    updated

--- Makefile
+++ Makefile
@@ -9,6 +9,8 @@ all: header.html body.md footer.html
 	cat header.html body.html footer.html > index.html
 	sed -i 's|href="http|target="_blank" href="http|g' index.html
 	cat status.md body.md > README.md
+	cp -r images style.css publications; ./list.sh publications
+	cp -r images style.css slides; ./list.sh slides
 
 clean:
 	cd ${workspace}/cv; make clean
---
+++ list.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ROOT=$1
+OUTPUT=`mktemp`
+
+cd $ROOT
+rm -f $OUTPUT
+touch $OUTPUT
+
+echo "<p>This is a list of files in this directory.</p>" >> $OUTPUT
+i=0
+filepath="."
+
+path=`basename "$filepath"`
+echo "  <h2>$path</h2>" >> $OUTPUT
+echo "  <ul>" >> $OUTPUT
+for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f | sort`; do
+file=`basename "$i"`
+echo "    <li><a href=\"$filepath/$file\">$file</a></li>" >> $OUTPUT
+done
+echo "  </ul>" >> $OUTPUT
+echo "  <br />" >> $OUTPUT
+echo "  <br />" >> $OUTPUT
+
+cat ../header.html $OUTPUT ../footer.html > index.html
+rm -f $OUTPUT
