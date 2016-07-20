e1102e320b1ae3dd164c7cc11899c86c0c870ed4
Aijun Bai <aijunbai@gmail.com>
2016-07-20 12:21:41 -0700

    updated

--- list.sh
+++ list.sh
@@ -11,7 +11,7 @@ echo "<p>This is a list of files in this directory.</p>" >> $OUTPUT
 
 filepath="."
 path=`basename "$filepath"`
-echo "  <h2>$path</h2>" >> $OUTPUT
+#echo "  <h2>$path</h2>" >> $OUTPUT
 echo "  <ul>" >> $OUTPUT
 for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f | sort`; do
 file=`basename "$i"`
